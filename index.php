e354a95482027d98465b81ae0cfe047c4ba0bd8d
fauziaameeri <ameeri.fauzia@student.greenriver.edu>
2022-04-07 13:59:28 -0400

    add h2

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h1> Hello world </h1>
+<h2> hahah hahaha</h2>
+
+</body>
+</html>
